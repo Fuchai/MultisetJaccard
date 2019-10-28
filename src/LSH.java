996c28ecaa263681847ad31580f4554c8dce925f
fuchai <huyaojie168@me.com>
2019-10-28 18:34:43 -0500

    Changed preprocessing interface with a constructor that takes folder name and changed process to take filename instead of absolute path. Reverted all absolute paths change from the last commit.

--- src/LSH.java
+++ src/LSH.java
@@ -1,6 +1,13 @@
 public class LSH {
-    public LSH(int[][] minHashMatrix, String[] docNames, int bands){
 
+    int[][] mhm;
+    String[] docNames;
+    int bands;
+
+    public LSH(int[][] minHashMatrix, String[] docNames, int bands){
+        this.mhm=minHashMatrix;
+        this.docNames=docNames;
+        this.bands=bands;
     }
 
     public String[] nearDuplicatesOf(String docName){
